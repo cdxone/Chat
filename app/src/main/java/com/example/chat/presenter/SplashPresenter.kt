46379fda9c9caf8dd608a7a48f7708e2d0808848
chendexiang <937682896@qq.com>
2021-08-09 12:35:34 +0800

    判断是否登录逻辑。

--- app/src/main/java/com/example/chat/presenter/SplashPresenter.kt
+++ app/src/main/java/com/example/chat/presenter/SplashPresenter.kt
@@ -1,6 +1,8 @@
 package com.example.chat.presenter
 
 import com.example.chat.contract.SplashContract
+import com.hyphenate.chat.EMClient
+import com.hyphenate.chat.EMOptions
 
 class SplashPresenter(private val view: SplashContract.View) : SplashContract.Presenter {
 
@@ -8,6 +10,8 @@ class SplashPresenter(private val view: SplashContract.View) : SplashContract.Pr
         if (isLoggedIn()) view.onLoggedIn() else view.onNotLoggedIn()
     }
 
-    private fun isLoggedIn(): Boolean = false
+    private fun isLoggedIn(): Boolean {
+        return EMClient.getInstance().isConnected && EMClient.getInstance().isLoggedInBefore
+    }
 
 }
