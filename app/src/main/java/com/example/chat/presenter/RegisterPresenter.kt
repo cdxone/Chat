969a72250f7e1cc406406092e37c54a8d6a3c833
chendexiang <937682896@qq.com>
2021-08-10 17:02:01 +0800

    注冊环信成功。

--- app/src/main/java/com/example/chat/presenter/RegisterPresenter.kt
+++ app/src/main/java/com/example/chat/presenter/RegisterPresenter.kt
@@ -6,6 +6,9 @@ import cn.bmob.v3.listener.SaveListener
 import com.example.chat.contract.RegisterContract
 import com.example.chat.extentions.isValidName
 import com.example.chat.extentions.isValidPsd
+import com.hyphenate.chat.EMClient
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 
 
 class RegisterPresenter(val view: RegisterContract.View) : RegisterContract.Presenter {
@@ -34,7 +37,7 @@ class RegisterPresenter(val view: RegisterContract.View) : RegisterContract.Pres
             // ❤ 这就相当于是Model层做的事情
             //注册失败会抛出HyphenateException
             // EMClient.getInstance().createAccount(name, psd);//同步方法
-            registerBmob();
+            registerBmob(name, psd);
         } catch (e: Exception) {
             // 注册失败
             view.onRegisterFailed()
@@ -46,21 +49,49 @@ class RegisterPresenter(val view: RegisterContract.View) : RegisterContract.Pres
 
     }
 
-    private fun registerBmob() {
+    private fun registerBmob(name: String, psd: String) {
         // 注册Bomb
         val user = BmobUser()
-        user.username = "cdx" + System.currentTimeMillis()
-        user.setPassword("123cdx")
+        user.username = name
+        user.setPassword(psd)
         user.signUp(object : SaveListener<BmobUser>() {
             override fun done(t: BmobUser?, e: BmobException?) {
                 if (e == null) {
                     // 注册到环信
+                    registerEasyMob(name, psd);
+                    //注册失败会抛出HyphenateException
+                    //EMClient.getInstance().createAccount(username, pwd);//同步方法
                 } else {
                     view.onRegisterFailed()
                 }
             }
 
         })
+    }
+
+    /**
+     * 注册到环信
+     */
+    private fun registerEasyMob(name: String, psd: String) {
+        //注册失败会抛出HyphenateException
+        // 注册环信
+        // 将代码段放在子线程中执行
+        doAsync {
+            try {
+                EMClient.getInstance().createAccount(name, psd) //同步方法
+                // 注冊成功
+                // 切换到主线程中运行
+                uiThread {
+                    view.onRegisterSuccess()
+                }
+            } catch (e: java.lang.Exception) {
+                // 注冊失敗
+                // 切换到主线程中运行
+                uiThread {
+                    view.onRegisterFailed()
+                }
+            }
+        }
     }
 
 }
